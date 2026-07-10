8a252fa8e837733e3c14d84ccc855cdcc354e7ef
sriguruparambarai-svg <sriguruparambarai@gmail.com>
2026-07-10 13:33:56 +0530

    Add files via upload

--- api/voice-class.js
+++ api/voice-class.js
@@ -40,6 +40,7 @@ function cleanText(text) {
   return String(text)
     .replace(/\[Page\s+\d+\]/gi, '')        // Remove [Page N] markers
     .replace(/♦/g, '')                       // Remove ♦ symbols
+    .replace(/[\u{1F000}-\u{1FAFF}\u{2600}-\u{27BF}\u{2B00}-\u{2BFF}\u{FE0F}\u{200D}\u{2190}-\u{21FF}\u{25A0}-\u{25FF}\u{2022}\u{2023}\u{2043}]/gu, ' ')  // Remove ALL emojis & picture symbols (🔍 ⭐ ► • etc.)
     .replace(/\*+/g, '')                     // Remove asterisks (markdown)
     .replace(/#/g, '')                       // Remove hashes
     .replace(/\.{3,}/g, '. ')                // Replace ... with single period
